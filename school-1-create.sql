d73ad4f4d25d879c1d00c6d6aafe27848c69ef13
sarahwade11 <sarah.wade@kellogg.northwestern.edu>
2023-01-17 20:11:57 +0000

    school changes

--- school-1-create.sql
+++ school-1-create.sql
@@ -1,5 +1,9 @@
 -- Create the students table
+-- Drop the table 
 
+DROP TABLE IF EXISTS students;
+
+--Recreate it
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
---
+++ school-5-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO students (
+    "first_name",
+    "last_name",
+    "email")
+    VALUES    
+    ("Ben",
+    "Block",
+    "ben_block@kellogg.northwestern.edu")
